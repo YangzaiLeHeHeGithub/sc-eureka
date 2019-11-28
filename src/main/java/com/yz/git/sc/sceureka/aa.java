4038b560bba02324560011d856cb53b496a79bb3
xuyang <1783708288@qq.com>
2019-11-28 11:18:29 +0800

    测试错误commit

---
+++ src/main/java/com/yz/git/sc/sceureka/aa.java
@@ -0,0 +1,27 @@
+package com.yz.git.sc.sceureka;
+
+import java.util.Objects;
+
+/**
+ * @description: TODO
+ * @title: aa
+ * @projectName： sc-eureka
+ * @author： xuyang
+ * @date： 2019/11/5/10:54
+ * @version： 1.0
+ */
+public class aa {
+
+    public static void main(String[] args) {
+
+        System.out.println(Objects.equals(new Integer(1),1));
+
+
+        Long  a1 = 1000L;
+        Long a2 = new Long(1000);
+
+        System.out.println(Objects.equals(a1,a2));
+        System.out.println(a1.longValue() == a2.longValue());
+        System.out.println(a1.longValue() == a2.longValue());
+    }
+}
